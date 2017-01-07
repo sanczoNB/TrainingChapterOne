1e1003ad34fdd68e3dc07b3a1c15f4297e860929
sanczoNB <sanczo16@gmail.com>
2017-01-07 18:25:19 +0100

    proste użycie formsów

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -7,8 +7,8 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Formatowanie liczb:\n\t {0}, {0:c}, {0:p3}, {0:e3}, \n\t{0:f3}," +
-                              " {0:g1}, {0:n10}, {0:r}", 0.123456789);
+            Console.WriteLine("Hello World!");
+            MessageBox.Show("Witaj, Świecie!");
         }
     }
 }
